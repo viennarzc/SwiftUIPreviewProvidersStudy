532fbe94befdf547240ff327b645d187a2d55a3a
Viennarz Curtiz <viennarzc@gmail.com>
2024-04-23 08:13:55 +0800

    Create project

---
+++ PreviewProviderStudy/PreviewProviderStudy/PreviewProviderStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PreviewProviderStudyApp.swift
+//  PreviewProviderStudy
+//
+//  Created by Viennarz Curtiz on 4/23/24.
+//
+
+import SwiftUI
+
+@main
+struct PreviewProviderStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
